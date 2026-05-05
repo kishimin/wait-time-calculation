81e65d574a3ea445d5388aae2986235e9902455a
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-05 21:24:14 +0900

    chore: マイグレーション設定を追加

--- backend/WaitTimeCalculationApi/Program.cs
+++ backend/WaitTimeCalculationApi/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WaitTimeCalculationApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseMySql(conn, ServerVersion.AutoDetect(conn));
+});
 
 var app = builder.Build();
 
