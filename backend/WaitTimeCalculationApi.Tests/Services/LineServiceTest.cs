272518970a8b87090ff51d852323f85eecaf5fbf
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-06 16:54:33 +0900

    feat:行列の一覧が取得できる(Service)
    層分離と行列の一覧を取得するため

---
+++ backend/WaitTimeCalculationApi.Tests/Services/LineServiceTest.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WaitTimeCalculationApi.Tests.Services
+{
+    public class LineServiceTest
+    {
+        [Fact]
+        public void 行列の一覧が取得できる()
+        {
+            // Given
+            // LineRepositoryをモック
+
+            // When
+            // GetAllAsync()
+
+            // Then
+            // モックした返り値とGetAllAsync()の返り値が一致することを確認
+        }
+    }
+}
