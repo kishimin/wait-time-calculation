b6e2d84912d0b7a20886eb1282e46f4c606f808a
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-07 21:54:39 +0900

    feat: Identityを使用できるように設定を追加

--- backend/WaitTimeCalculationApi/Data/ApplicationDbContext.cs
+++ backend/WaitTimeCalculationApi/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WaitTimeCalculationApi.Models;
 
@@ -15,5 +16,28 @@ namespace WaitTimeCalculationApi.Data
         }
 
         public DbSet<Line> Lines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "admin-role-id",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "user-role-id",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
