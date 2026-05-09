7860a7a88af1cfc7a4b642e6a44bfee5589ac824
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-09 19:02:11 +0900

    fix:UTCでもバグとならないように修正
    Nowではなく、utcNowを使用するようにした

--- backend/WaitTimeCalculationApi/Services/TokenService.cs
+++ backend/WaitTimeCalculationApi/Services/TokenService.cs
@@ -37,7 +37,7 @@ namespace WaitTimeCalculationApi.Services
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
+                Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = creds,
                 Issuer = _config["JWT:Issuer"],
                 Audience = _config["JWT:Audience"]
