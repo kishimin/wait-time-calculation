c0dadd02e186febb2d734ab21bcafb636ecfb401
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-13 22:02:45 +0900

    fix: 読み取り専用のため、AsNoTracking()を追加、投影型をLineEntryRepositoryが返すように変更
    また、負荷を小さくするため

--- backend/WaitTimeCalculationApi/Interfaces/ILineEntryRepository.cs
+++ backend/WaitTimeCalculationApi/Interfaces/ILineEntryRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WaitTimeCalculationApi.Models;
+using WaitTimeCalculationApi.Projections;
 
 namespace WaitTimeCalculationApi.Interfaces
 {
@@ -10,8 +11,7 @@ namespace WaitTimeCalculationApi.Interfaces
     {
         Task<LineEntry> CreateAsync(LineEntry lineEntryModel);
 
-        // userIdから、LineEntryの中で、UpdatedAtが最新のもののみ取得
-        Task<LineEntry?> GetLatestUpdatedAsync(string userId);
+        Task<CurrentEntryInfo?> GetCurrentEntryAsync(string userId);
 
         Task<LineEntry?> UpdateAsync(Guid id, string userId, LineEntry lineEntryModel);
 
