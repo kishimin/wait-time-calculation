73723e138dfb9929eae4091b8345994875acde66
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-13 22:08:46 +0900

    fix: LineServiceのGetAllAsyncをユーザー依存が伝わるGetAllForUserAsync()に変更

--- backend/WaitTimeCalculationApi/Interfaces/ILineService.cs
+++ backend/WaitTimeCalculationApi/Interfaces/ILineService.cs
@@ -10,7 +10,7 @@ namespace WaitTimeCalculationApi.Interfaces
 {
     public interface ILineService
     {
-        Task<List<LinesResult>> GetAllAsync(string userId);
+        Task<List<LinesResult>> GetAllForUserAsync(string userId);
 
         Task<Line?> GetByIdAsync(Guid id);
 
