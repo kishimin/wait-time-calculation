1f4736029c4e76a64089848ce6bf690c128b0f30
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-15 07:28:28 +0900

    fix: test-result.jsonが出力されるようにした

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,12 @@
+/// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    reporters: ["default", "json"],
+    outputFile: "test-result.json",
+  },
 });
