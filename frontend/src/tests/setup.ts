e0d873b40ce68328995ffe48dc95f1b534e699da
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-16 14:28:36 +0900

    chore: mswの設定を追加

--- frontend/src/tests/setup.ts
+++ frontend/src/tests/setup.ts
@@ -1,1 +1,14 @@
 import "@testing-library/jest-dom/vitest";
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+import { server } from "../api/mocks/server";
+
+beforeAll(() => server.listen());
+
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+
+afterAll(() => server.close());
