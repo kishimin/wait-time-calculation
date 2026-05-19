45c225ae97f2cf1067eaca3e9a9d42b51bb5ef58
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-19 21:27:59 +0900

    feat: 空の時、エラーが表示されるを追加

--- frontend/src/features/create-line/views/create-line.tsx
+++ frontend/src/features/create-line/views/create-line.tsx
@@ -1,11 +1,26 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { TextField } from "@mui/material";
 import { useForm } from "react-hook-form";
+import { formSchema, type FormSchema } from "../schemas/form";
 
 const CreateLine = () => {
-  const { register } = useForm();
+  const {
+    register,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    mode: "onChange",
+  });
+
   return (
     <>
-      <TextField label={"タイトル"} required />
+      <TextField
+        {...register("title")}
+        label={"タイトル"}
+        required
+        error={!!errors.title}
+        helperText={errors.title?.message}
+      />
     </>
   );
 };
