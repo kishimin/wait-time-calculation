df4b2cbb899ea4694a80843b90d359e5cbf9ce1f
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-20 22:33:10 +0900

    feat: タイトルを入力して、作成ボタンをクリックするとローディングが表示される

--- frontend/src/features/create-line/views/create-line.tsx
+++ frontend/src/features/create-line/views/create-line.tsx
@@ -1,6 +1,12 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, TextField } from "@mui/material";
-import { useForm } from "react-hook-form";
+import {
+  Button,
+  CircularProgress,
+  FormControl,
+  TextField,
+} from "@mui/material";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { usePostApiLine } from "../../../api/endpoints/line/line";
 import { formSchema } from "../schemas/form";
 import type { FormSchema } from "../types/form";
 
@@ -8,29 +14,47 @@ const CreateLine = () => {
   const {
     register,
     formState: { errors },
+    handleSubmit,
   } = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     mode: "onChange",
   });
 
+  const { isPending, mutate } = usePostApiLine();
+
+  const onSubmit: SubmitHandler<FormSchema> = (data) => {
+    mutate({ data });
+  };
+
   return (
     <>
-      <TextField
-        {...register("title")}
-        label={"タイトル"}
-        required
-        error={!!errors.title}
-        helperText={errors.title?.message}
-      />
+      {isPending ? (
+        <CircularProgress />
+      ) : (
+        <FormControl
+          component={"form"}
+          onSubmit={(e) => {
+            return void handleSubmit(onSubmit)(e);
+          }}
+        >
+          <TextField
+            {...register("title")}
+            label={"タイトル"}
+            required
+            error={!!errors.title}
+            helperText={errors.title?.message}
+          />
 
-      <TextField
-        {...register("explanation")}
-        label={"説明"}
-        error={!!errors.explanation}
-        helperText={errors.explanation?.message}
-      />
+          <TextField
+            {...register("explanation")}
+            label={"説明"}
+            error={!!errors.explanation}
+            helperText={errors.explanation?.message}
+          />
 
-      <Button>{"作成"}</Button>
+          <Button type={"submit"}>{"作成"}</Button>
+        </FormControl>
+      )}
     </>
   );
 };
