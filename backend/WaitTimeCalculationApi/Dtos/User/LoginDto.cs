6c56405871a6cb758d34a44f7eaf99da4dc259e8
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-01 21:02:06 +0900

    fix: userName、passwordの文字数に制約をつけた
    フロントでUX向上のための制約をつけるため

--- backend/WaitTimeCalculationApi/Dtos/User/LoginDto.cs
+++ backend/WaitTimeCalculationApi/Dtos/User/LoginDto.cs
@@ -9,9 +9,11 @@ namespace WaitTimeCalculationApi.Dtos.User
     public class LoginDto
     {
         [Required]
+        [MaxLength(50, ErrorMessage = "ユーザー名は50文字以下です")]
         public string Username { get; set; } = string.Empty;
 
         [Required]
+        [MaxLength(100, ErrorMessage = "パスワードは100文字以下です")]
         public string Password { get; set; } = string.Empty;
     }
 }
