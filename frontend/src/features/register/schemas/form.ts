33ded288a134ee3a6db58ee2695119f75cd7de48
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-02 21:50:18 +0900

    feat: 51文字以上の時エラーが表示される

--- frontend/src/features/register/schemas/form.ts
+++ frontend/src/features/register/schemas/form.ts
@@ -1,5 +1,8 @@
 import * as z from "zod";
 
 export const formSchema = z.object({
-  userName: z.string().min(1, "ユーザー名は必須です"),
+  userName: z
+    .string()
+    .min(1, "ユーザー名は必須です")
+    .max(50, "ユーザー名は50文字以内で入力してください"),
 });
