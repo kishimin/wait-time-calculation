43049c56aee12663cc0763e891c15b4057b55b56
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-06 21:49:58 +0900

    test: 入力項目にエラーが無い時に新規登録ボタンをクリックするとローディングが表示される

---
+++ frontend/src/features/register/tests/register.medium.test.tsx
@@ -0,0 +1,40 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { delay } from "msw";
+import { getPostApiUserRegisterMockHandler } from "../../../api/endpoints/user/user.msw";
+import { server } from "../../../api/mocks/server";
+import Register from "../views/register";
+import { getEmailInput, getPasswordInput, getUserNameInput } from "./helper";
+
+const setup = () => {
+  const user = userEvent.setup();
+  const queryClient = new QueryClient();
+
+  render(
+    <QueryClientProvider client={queryClient}>
+      <Register />
+    </QueryClientProvider>,
+  );
+
+  return { user };
+};
+
+describe("新規登録", () => {
+  test("入力項目にエラーが無い時にクリックするとローディングが表示される", async () => {
+    const { user } = setup();
+    server.use(
+      getPostApiUserRegisterMockHandler(async () => {
+        await delay(1000);
+        return {};
+      }),
+    );
+
+    await user.type(getUserNameInput(), "a");
+    await user.type(getPasswordInput(), "aA1!".repeat(11));
+    await user.type(getEmailInput(), "a@gmail.com");
+    await user.click(screen.getByRole("button", { name: "新規登録" }));
+
+    expect(screen.getByRole("progressbar")).toBeVisible();
+  });
+});
