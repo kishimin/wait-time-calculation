522752d221fc346cc501d19a7d666f589998a8a3
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-06 23:12:53 +0900

    test:新規登録が成功すると一覧画面に遷移する

--- frontend/src/app/tests/router.test.tsx
+++ frontend/src/app/tests/router.test.tsx
@@ -43,6 +43,22 @@ describe("ルート", () => {
   });
 });
 
+describe("認証", () => {
+  test("新規登録が成功すると一覧画面に遷移する", async () => {
+    const { user } = setup("/register");
+
+    await user.type(screen.getByRole("textbox", { name: "ユーザー名" }), "a");
+    await user.type(screen.getByLabelText(/パスワード/), "aA1!".repeat(11));
+    await user.type(
+      screen.getByRole("textbox", { name: "メールアドレス" }),
+      "a@gmail.com",
+    );
+    await user.click(screen.getByRole("button", { name: "新規登録" }));
+
+    expect(await screen.findByRole("list")).toBeVisible();
+  });
+});
+
 describe("まちログ", () => {
   test("作成が成功すると、一覧画面に遷移する", async () => {
     const { user } = setup("/create");
