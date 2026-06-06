1bd647a07db9f4c0ccdb46d0d1c462c3b95667e0
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-06 21:58:23 +0900

    fix: register.small.test.tsxにもQueryClientProviderでラップするようにした
    api通信を行っているため2

--- frontend/src/features/register/tests/register.small.test.tsx
+++ frontend/src/features/register/tests/register.small.test.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Register from "../views/register";
@@ -6,8 +7,13 @@ import { getEmailInput, getPasswordInput, getUserNameInput } from "./helper";
 
 const setup = () => {
   const user = userEvent.setup();
+  const queryClient = new QueryClient();
 
-  render(<Register />);
+  render(
+    <QueryClientProvider client={queryClient}>
+      <Register />
+    </QueryClientProvider>,
+  );
 
   return { user };
 };
