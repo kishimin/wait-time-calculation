70a3ca984d08fb4ecbef73b025bad4bb4eb942c5
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-07 11:54:06 +0900

    feat: ログイン画面のルートは/loginである

--- frontend/src/app/routes/router.tsx
+++ frontend/src/app/routes/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import CreateLine from "../../features/create-line/views/create-line";
 import Lines from "../../features/lines/views/lines";
+import Login from "../../features/login/views/login";
 import Register from "../../features/register/views/register";
 import { PATHS } from "../../types/paths";
 
@@ -18,6 +19,7 @@ export const RouterContents = () => {
       <Route path={PATHS.index} element={<Lines />} />
       <Route path={PATHS.create} element={<CreateLine />} />
       <Route path={PATHS.register} element={<Register />} />
+      <Route path={PATHS.login} element={<Login />} />
     </Routes>
   );
 };
