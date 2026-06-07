c4651c2b7085da978e4508b086a511c0584e3131
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-07 11:49:24 +0900

    feat: ログインのAPI通信を行えるようにした

--- frontend/src/features/login/tests/setup.tsx
+++ frontend/src/features/login/tests/setup.tsx
@@ -1,11 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router";
+import { SnackbarContextProvider } from "../../../providers/snackbar";
 import Login from "../views/login";
 
 export const setup = () => {
   const user = userEvent.setup();
+  const queryClient = new QueryClient();
 
-  render(<Login />);
+  render(
+    <MemoryRouter>
+      <QueryClientProvider client={queryClient}>
+        <SnackbarContextProvider>
+          <Login />
+        </SnackbarContextProvider>
+      </QueryClientProvider>
+    </MemoryRouter>,
+  );
 
   return { user };
 };
--- frontend/src/features/login/views/login.tsx
+++ frontend/src/features/login/views/login.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import {
   Button,
+  CircularProgress,
   FormControl,
   IconButton,
   InputAdornment,
@@ -9,11 +10,21 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { useForm, type SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router";
+import { usePostApiUserLogin } from "../../../api/endpoints/user/user";
 import { PostApiUserLoginBody } from "../../../gen/endpoints/user/user.zod";
+import { useSnackbar } from "../../../hooks/use-snackbar";
+import { tokenSchema, UserSchema } from "../../../schemas/user";
+import { LOCAL_STORAGE_KEY } from "../../../types/localstorage";
+import { PATHS } from "../../../types/paths";
 import { formSchema } from "../schemas/login-form";
 import type { FormSchema } from "../types/login-form";
 
 const Login = () => {
+  const { toggleSnack } = useSnackbar();
+
+  const navigate = useNavigate();
+
   const {
     register,
     formState: { errors },
@@ -23,13 +34,40 @@ const Login = () => {
     mode: "onChange",
   });
 
+  const { isPending, mutate } = usePostApiUserLogin({
+    mutation: {
+      onSuccess: async (data) => {
+        try {
+          const userObj = UserSchema.parse({
+            email: data.email,
+            isLoggedIn: true,
+            userName: data.userName,
+          });
+
+          const token = tokenSchema.parse(data.token);
+
+          localStorage.setItem(LOCAL_STORAGE_KEY.TOKEN, token);
+          localStorage.setItem(LOCAL_STORAGE_KEY.USER, JSON.stringify(userObj));
+
+          await navigate(PATHS.index);
+        } catch {
+          toggleSnack({ message: "内部的なエラーです。再度お試しください。" });
+        }
+      },
+    },
+  });
+
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
   const onSubmit: SubmitHandler<FormSchema> = (data) => {
-    PostApiUserLoginBody.safeParse({
+    const result = PostApiUserLoginBody.safeParse({
       username: data.userName,
       password: data.password,
     });
+
+    if (result.success) {
+      mutate({ data: result.data });
+    }
   };
 
   const handleClickShowPassword = () => {
@@ -46,50 +84,55 @@ const Login = () => {
 
   return (
     <>
-      <FormControl
-        component={"form"}
-        onSubmit={(e) => {
-          return void handleSubmit(onSubmit)(e);
-        }}
-        noValidate
-      >
-        <TextField
-          {...register("userName")}
-          label={"ユーザー名"}
-          required
-          error={!!errors.userName}
-          helperText={errors.userName?.message}
-        />
-
-        <TextField
-          {...register("password")}
-          label={"パスワード"}
-          type={showPassword ? "text" : "password"}
-          required
-          error={!!errors.password}
-          helperText={errors.password?.message}
-          slotProps={{
-            input: {
-              endAdornment: (
-                <InputAdornment position={"end"}>
-                  <IconButton
-                    aria-label={
-                      showPassword ? "入力内容を非表示" : "入力内容を表示"
-                    }
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    onMouseUp={handleMouseUpPassword}
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            },
+      {isPending ? (
+        <CircularProgress />
+      ) : (
+        <FormControl
+          component={"form"}
+          onSubmit={(e) => {
+            return void handleSubmit(onSubmit)(e);
           }}
-        />
+          noValidate
+          aria-label={"ログインフォーム"}
+        >
+          <TextField
+            {...register("userName")}
+            label={"ユーザー名"}
+            required
+            error={!!errors.userName}
+            helperText={errors.userName?.message}
+          />
 
-        <Button type={"submit"}>{"ログイン"}</Button>
-      </FormControl>
+          <TextField
+            {...register("password")}
+            label={"パスワード"}
+            type={showPassword ? "text" : "password"}
+            required
+            error={!!errors.password}
+            helperText={errors.password?.message}
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position={"end"}>
+                    <IconButton
+                      aria-label={
+                        showPassword ? "入力内容を非表示" : "入力内容を表示"
+                      }
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      onMouseUp={handleMouseUpPassword}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+
+          <Button type={"submit"}>{"ログイン"}</Button>
+        </FormControl>
+      )}
     </>
   );
 };
