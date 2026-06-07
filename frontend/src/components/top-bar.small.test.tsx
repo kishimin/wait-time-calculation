f2f0289993d9aab5f147af170d0b1a12168e266b
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-07 15:30:45 +0900

    test:ヘッダーが表示される

--- frontend/src/components/top-bar.small.test.tsx
+++ frontend/src/components/top-bar.small.test.tsx
@@ -2,6 +2,12 @@ import { screen } from "@testing-library/dom";
 import { render } from "@testing-library/react";
 import { TopBar } from "./top-bar";
 
+test("ヘッダーが表示される", () => {
+  render(<TopBar />);
+
+  expect(screen.getByRole("banner")).toBeVisible();
+});
+
 test("まちログと表示される", () => {
   render(<TopBar />);
 
