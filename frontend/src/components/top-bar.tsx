02842cb4de137c5e74717c49c62dd19731c66054
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-07 15:27:59 +0900

    feat: まちログのログ画像が表示される

--- frontend/src/components/top-bar.tsx
+++ frontend/src/components/top-bar.tsx
@@ -1,9 +1,10 @@
-import { Typography } from "@mui/material";
+import { AppBar, Box, Typography } from "@mui/material";
 
 export const TopBar = () => {
   return (
-    <>
+    <AppBar position={"static"}>
       <Typography variant={"h1"}>{"まちログ"}</Typography>
-    </>
+      <Box component={"img"} src={"/QueueLogoImage.png"} />
+    </AppBar>
   );
 };
