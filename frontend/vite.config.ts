3f68c64d1b738c3bd3915522e647756e8a471252
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-17 21:28:07 +0900

    chore: storybookをインストールした
    デザインを整えるため

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,44 @@
 /// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/tests/setup.ts"],
-    reporters: process.env.GITHUB_ACTIONS
-      ? ["dot", "github-actions", "json"]
-      : ["dot"],
+    reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions", "json"] : ["dot"],
     outputFile: "test-result.json",
-  },
+    projects: [{
+      extends: true,
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["./src/tests/setup.ts"]
+      }
+    }, {
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
+  }
 });
