2baaa6519e76e0f3f576711c6399ad1fce6f34ea
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-30 07:30:40 +0900

    feat: 監視用のapiを作成

--- backend/WaitTimeCalculationApi/Program.cs
+++ backend/WaitTimeCalculationApi/Program.cs
@@ -102,6 +102,9 @@ builder.Services.AddScoped<ILineEntryService, LineEntryService>();
 
 var app = builder.Build();
 
+// monitor
+app.MapGet("/health", () => Results.Ok("OK"));
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
