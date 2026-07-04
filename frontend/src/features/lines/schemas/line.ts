feaaa0c9e34cd86350751ee93a8de9c9a1b92596
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-04 14:32:37 +0900

    fix: z.number().nullable()に変更
    整数以外も許容するため

--- frontend/src/features/lines/schemas/line.ts
+++ frontend/src/features/lines/schemas/line.ts
@@ -4,6 +4,6 @@ import { titleSchema } from "../../../schemas/line";
 export const LineSchema = z.object({
   id: z.uuid(),
   title: titleSchema,
-  averageWaitTime: z.int().nullable(),
+  averageWaitTime: z.number().nullable(),
   currentLineEntryId: z.uuid().nullable(),
 });
