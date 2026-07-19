a2bf3ab0847ae71a7b12a2dba5a8db98793f5252
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-19 11:40:52 +0900

    feat: baseUrlに変更

--- frontend/e2e/lines.spec.ts
+++ frontend/e2e/lines.spec.ts
@@ -1,18 +1,17 @@
 import { expect, test } from "@playwright/test";
-import { FRONTEND_URL } from "./types/constants";
 
 test("一覧ページが表示される", async ({ page }) => {
-  await page.goto(`${FRONTEND_URL}/`);
+  await page.goto("/");
 
   await expect(page.getByRole("heading", { name: "まちログ" })).toBeVisible();
   await expect(page.getByRole("list")).toBeVisible();
 });
 
 test("新規登録画面への遷移", async ({ page }) => {
-  await page.goto(`${FRONTEND_URL}/`);
+  await page.goto("/");
 
   await page.getByRole("link", { name: "新規登録" }).click();
 
   await expect(page.getByRole("button", { name: "新規登録" })).toBeVisible();
-  await expect(page).toHaveURL(`${FRONTEND_URL}/register`);
+  await expect(page).toHaveURL("/register");
 });
