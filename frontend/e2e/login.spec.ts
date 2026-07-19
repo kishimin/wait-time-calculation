8f50a9a1e13485799258c9b7fe94bd185359b035
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-19 11:41:14 +0900

    feat: ログイン→一覧画面への遷移のテスト追加2

--- frontend/e2e/login.spec.ts
+++ frontend/e2e/login.spec.ts
@@ -1,8 +1,19 @@
 import { expect, test } from "@playwright/test";
-import { FRONTEND_URL } from "./types/constants";
 
 test("ログイン画面が表示される", async ({ page }) => {
-  await page.goto(`${FRONTEND_URL}/login`);
+  await page.goto("/login");
 
-  await expect(page.getByRole("button", {name: "ログイン"})).toBeVisible()
+  await expect(page.getByRole("button", { name: "ログイン" })).toBeVisible();
+});
+
+test("ログインして一覧画面に遷移する", async ({ page }) => {
+  await page.goto("/login");
+
+  await page.getByRole("textbox", { name: "ユーザー名" }).fill("userName");
+  await page.getByLabel("パスワード").fill("Password123!");
+  await page.getByRole("button", { name: "ログイン" }).click();
+
+  await expect(page).toHaveURL("/");
+
+  await expect(page.getByRole("list")).toBeVisible();
 });
