9a79b7db8e535af9f6ba7a228a1dea2cc6faa8d3
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-19 20:29:04 +0900

    feat: APIをモックしたときのスクリーンショット、VRTを追加

--- frontend/src/features/lines/tests/lines.playwright.spec.ts
+++ frontend/src/features/lines/tests/lines.playwright.spec.ts
@@ -17,4 +17,12 @@ test("一覧のタイトルが表示される", async ({ page }) => {
       .filter({ has: page.getByRole("listitem") })
       .first(),
   ).toHaveText(/タイトル/);
+
+  // スクリーンショット
+  await page.screenshot({ path: "screenshot/login_mock.png" });
+
+  // VRT
+  await expect(page).toHaveScreenshot();
+
+  // ビデオ撮影
 });
