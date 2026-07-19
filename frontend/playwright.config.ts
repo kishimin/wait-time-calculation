70faee742871a7d82058c46ff3e8faa04d169386
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-19 16:09:29 +0900

    feat:Chromeのみ動画撮影_

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -36,7 +36,12 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        video: {
+          mode: "on",
+        },
+      },
     },
 
     {
