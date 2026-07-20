3ae094483e59f008eebb2de339f9bd9c0a40d3c0
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-20 11:20:33 +0900

    feat: top-bar.tsを使用2

--- frontend/e2e/components/top-bar.ts
+++ frontend/e2e/components/top-bar.ts
@@ -4,12 +4,14 @@ import { topBarTexts } from "../constants/components";
 /** トップバー */
 export class TopBar {
   readonly page: Page;
+  readonly getBanner: Locator;
   readonly getLogoutButton: Locator;
 
   /** componentの初期化 */
   constructor(page: Page) {
     this.page = page;
-    this.getLogoutButton = page.getByRole("button", {
+    this.getBanner = page.getByRole("banner");
+    this.getLogoutButton = page.getByRole("banner").getByRole("button", {
       name: topBarTexts.logoutButton,
     });
   }
--- frontend/e2e/specs/login.spec.ts
+++ frontend/e2e/specs/login.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "@playwright/test";
-import { topBarTexts } from "../constants/components";
+import { TopBar } from "../components/top-bar";
 import { linesTexts } from "../constants/lines";
 import { LoginPage } from "../pages/login-page";
 
@@ -20,24 +20,23 @@ test("ログイン画面が表示される", async ({ page }) => {
 
 test("ログインすると一覧画面に遷移する", async ({ page }) => {
   const loginPage = new LoginPage(page);
+  const topBar = new TopBar(page);
   await loginPage.goto();
 
   await loginPage.login();
 
   await expect(page).toHaveURL(linesTexts.link);
 
-  await expect(
-    page.getByRole("button", { name: topBarTexts.logoutButton }),
-  ).toBeVisible();
+  await expect(topBar.getLogoutButton).toBeVisible();
   await expect(page.getByRole("list")).toBeVisible();
 
   await page.screenshot({
     path: "screenshot/lines_certified_full_page.png",
     fullPage: true,
   });
-  await page
-    .getByRole("banner")
-    .screenshot({ path: "screenshot/banner_certified.png" });
+  await topBar.getBanner.screenshot({
+    path: "screenshot/banner_certified.png",
+  });
 
   await expect(page).toHaveScreenshot();
 });
